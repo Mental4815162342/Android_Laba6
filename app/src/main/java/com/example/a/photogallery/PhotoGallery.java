f565f3936690df5c0cc2d8fffa356aa32939490a
Mental4815162342 <bamail1999@gmail.com>
2020-12-19 16:50:03 +0700

    11. Добавьте в адаптер RecyclerView

--- app/src/main/java/com/example/a/photogallery/PhotoGallery.java
+++ app/src/main/java/com/example/a/photogallery/PhotoGallery.java
@@ -5,8 +5,22 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
-public class PhotoGallery extends AppCompatActivity {
+import com.example.a.photogallery.api.FlickrAPI;
+import com.example.a.photogallery.api.ServiceAPI;
+import com.example.a.photogallery.db.PhotosDB;
+import com.example.a.photogallery.model.Example;
+import com.example.a.photogallery.model.Photo;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+public class PhotoGallery extends AppCompatActivity implements PhotoAdapter.OnInsertListener {
+
+    private PhotosDB db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +28,27 @@ public class PhotoGallery extends AppCompatActivity {
         setContentView(R.layout.gallery_activity);
         final RecyclerView rv = findViewById(R.id.rView);
         rv.setLayoutManager(new GridLayoutManager(this, 3));
+        db = PhotosDB.getDatabase(getApplicationContext());
+
+        Retrofit r = ServiceAPI.getRetrofit();
+        r.create(FlickrAPI.class).getRecent().enqueue(new Callback<Example>() {
+            @Override
+            public void onResponse(Call<Example> call, Response<Example> response) {
+                response.body();
+                Log.v("b", ((Example) response.body()).getPhotos().getTotal());
+                PhotoAdapter pa = new PhotoAdapter(this, response.body().getPhotos().getPhoto());
+                pa.setOnInsertListener(PhotoGallery.this);
+                rv.setAdapter(pa);
+            }
+            @Override
+            public void onFailure(Call<Example> call, Throwable t) {
+            }
+        });
+
+    }
+
+    @Override
+    public void onInsert(Photo photo) {
+        db.getPhotoDao().insertPhoto(photo);
     }
 }
